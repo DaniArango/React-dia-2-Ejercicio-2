3fb8e0d06fff115d734c777043d9df8d3be67e59
[DaniArango] <[daniarangop93@gmail.com]>
2022-11-22 14:18:40 +0100

    first commit

---
+++ src/components/Counter/Counter.jsx
@@ -0,0 +1,19 @@
+import { useState } from "react";
+import "./Counter.css"
+const Counter = () => {
+    const [counter, setCounter] = useState(0);
+    const increment = () => setCounter(counter + 1);
+    const decrement = () => setCounter(counter - 1);
+    const reset = () => setCounter(0);
+
+    return (
+        <div>{counter}
+       
+        <button className="button" onClick={increment}>+</button>
+        <button className="button" onClick={decrement}>-</button>
+        <button className="button" onClick={reset}>Zero</button>
+        </div>
+    )
+};
+
+export default Counter
